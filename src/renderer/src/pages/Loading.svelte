1c7eca876a0af5b1a11a60a53d07381e4348cc98
Importantus <Importantus@users.noreply.github.com>
2024-12-03 16:32:03 +0100

    Add loading page

---
+++ src/renderer/src/pages/Loading.svelte
@@ -0,0 +1,5 @@
+<div class="flex h-screen w-screen flex-col justify-center items-center px-6 py-12 lg:px-8">
+  <div
+    class="size-10 border-4 border-brand-500 border-t-transparent rounded-full animate-spin"
+  ></div>
+</div>
