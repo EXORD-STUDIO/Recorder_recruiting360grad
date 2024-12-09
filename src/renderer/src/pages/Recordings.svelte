32a8583ee03cfd3961daf0397e891d61393d000a
Importantus <Importantus@users.noreply.github.com>
2024-12-09 16:28:16 +0100

    Show date in recordings list

--- src/renderer/src/pages/Recordings.svelte
+++ src/renderer/src/pages/Recordings.svelte
@@ -14,7 +14,16 @@
 
 <Base currentlyActive="recordings">
   <div class="flex flex-col gap-2 p-2">
-    {#each recordings as recording}
+    {#each recordings as recording, i}
+      {#if i > 0 && recordings[i - 1].createdAt - recording.createdAt > 1000 * 60 * 60 * 24}
+        <div class="w-full flex gap-2 items-center">
+          <div class="w-full border-b border-gray-300"></div>
+          <h1 class="font-semibold">
+            {new Date(recording.createdAt).toLocaleDateString()}
+          </h1>
+          <div class="w-full border-b border-gray-300"></div>
+        </div>
+      {/if}
       <RecordingItem {recording} />
     {/each}
   </div>
