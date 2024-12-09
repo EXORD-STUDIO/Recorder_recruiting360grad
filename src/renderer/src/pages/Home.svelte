67013c284a22ab167da2dde9757740a57b589feb
Importantus <Importantus@users.noreply.github.com>
2024-12-09 16:28:38 +0100

    Make home page

--- src/renderer/src/pages/Home.svelte
+++ src/renderer/src/pages/Home.svelte
@@ -2,10 +2,15 @@
   import { onMount } from 'svelte'
   import InputSelect from '../components/input/InputSelect.svelte'
   import Base from '../layouts/Base.svelte'
-  import { currentRecording, startRecording, stopRecording } from '../stores/recorder.store'
+  import { currentRecording, startRecording } from '../stores/recorder.store'
+  import { currentContact } from '../stores/currentContact.store'
+  import InputText from '../components/input/InputText.svelte'
+  import InputToggle from '../components/input/InputToggle.svelte'
+  import { getContactName } from '../utils/formatters'
 
   let micId = 'default'
   let possibleMics = []
+  let name = 'DSGVO'
 
   async function getMics(): Promise<void> {
     possibleMics = await navigator.mediaDevices.enumerateDevices().then((devices) => {
@@ -27,40 +32,57 @@
 
 <Base currentlyActive="start">
   <div class="h-full">
-    <h1>Home</h1>
-    <p>Welcome to the home page.</p>
-    <div class="pt-10">Recording is {$currentRecording ? 'running' : 'not running'}</div>
-    <div>
-      <InputSelect
-        bind:value={micId}
-        options={possibleMics}
-        placeholder="Select a microphone"
-        onSelect={(value) => {
-          micId = value
-        }}
-      />
-    </div>
-    <div class="flex p-4 gap-2">
-      <button
-        class="btn btn-primary"
-        on:click={() =>
-          startRecording({
-            desktopId: '',
-            micId: '',
-            contact: null
-          })}>Start Recording</button
-      >
-      <button
-        class="btn btn-primary"
-        on:click={() => {
-          stopRecording($currentRecording)
-        }}>Stop Recording</button
-      >
-    </div>
-    {#if $currentRecording}
-      <div class="p-4">
-        <p>Size: {$currentRecording.size}</p>
-        <p>Länge: {$currentRecording.length}</p>
+    {#if !$currentRecording}
+      <div class="p-3 flex flex-col gap-5">
+        <InputSelect
+          bind:value={micId}
+          options={possibleMics}
+          label="Mikrofon"
+          placeholder="Select a microphone"
+          onSelect={(value) => {
+            micId = value
+          }}
+        />
+        {#if $currentContact}
+          <div>
+            <label for={name} class="block text-sm font-medium leading-6 text-gray-900 mb-2"
+              >Ausgewählter Kontakt</label
+            >
+            <div class="bg-brand-50 rounded-md flex flex-col p-2">
+              <div>
+                <span class="text-sm font-medium leading-6 text-gray-900"
+                  >{getContactName($currentContact)}</span
+                >
+                <span class="text-sm text-gray-500">{$currentContact.reg_no}</span>
+              </div>
+            </div>
+            <p class="mt-2 text-sm text-gray-500" id="email-description">
+              Um einen anderen Kontakt auszuwählen, öffnen Sie ihn in der Web-App.
+            </p>
+          </div>
+        {/if}
+        <InputText bind:value={name} placeholder="Name" label="Name der Aufzeichnung" name="name" />
+        <InputToggle
+          title="DSGVO"
+          description="Es handelt sich um eine Aufzeichnung zur Einwilligung nach DSGVO."
+          checked={name === 'DSGVO'}
+          onChange={(checked) => {
+            name = checked ? 'DSGVO' : ''
+          }}
+        />
+        <button
+          class="btn btn-primary"
+          on:click={() =>
+            startRecording({
+              name: name,
+              micId: micId,
+              contact: $currentContact
+            })}>Start Recording</button
+        >
+      </div>
+    {:else if $currentRecording}
+      <div class="p-3 flex flex-col gap-5 h-full w-full justify-center items-center">
+        Aufzeichnung für {getContactName($currentContact)} läuft.
       </div>
     {/if}
   </div>
