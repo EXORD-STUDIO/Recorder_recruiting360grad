c125fa79c766c69fb2a8c4b970120c888c2190d5
Importantus <Importantus@users.noreply.github.com>
2024-12-09 16:27:43 +0100

    Add dialog to sign out

--- src/renderer/src/layouts/Base.svelte
+++ src/renderer/src/layouts/Base.svelte
@@ -5,6 +5,7 @@
   import { navigateTo } from '../utils/navigate'
   import { bytesToMegaBytesString, getContactName, lengthToString } from '../utils/formatters'
   import { dialogs } from '../components/dialogs/dialogs.store'
+  import { logout } from '../auth/pocketbase'
   import Dialogs from '../components/dialogs/Dialogs.svelte'
 
   export let currentlyActive: 'recordings' | 'start'
@@ -21,6 +22,19 @@
       func: () => navigateTo('/recordings')
     }
   ]
+
+  function logoutDialog() {
+    dialogs.add({
+      title: 'Abmelden',
+      message: 'Möchten Sie sich wirklich abmelden?',
+      cancelText: 'Abbrechen',
+      confirmText: 'Abmelden',
+      onConfirm: () => {
+        logout()
+        navigateTo('/login')
+      }
+    })
+  }
 </script>
 
 <Dialogs />
@@ -34,7 +48,7 @@
         <p class="text-sm text-gray-500 -mt-1">{$user?.email}</p>
       </div>
     </div>
-    <button>
+    <button on:click={logoutDialog}>
       <svg
         xmlns="http://www.w3.org/2000/svg"
         fill="none"
@@ -46,12 +60,7 @@
         <path
           stroke-linecap="round"
           stroke-linejoin="round"
-          d="M9.594 3.94c.09-.542.56-.94 1.11-.94h2.593c.55 0 1.02.398 1.11.94l.213 1.281c.063.374.313.686.645.87.074.04.147.083.22.127.325.196.72.257 1.075.124l1.217-.456a1.125 1.125 0 0 1 1.37.49l1.296 2.247a1.125 1.125 0 0 1-.26 1.431l-1.003.827c-.293.241-.438.613-.43.992a7.723 7.723 0 0 1 0 .255c-.008.378.137.75.43.991l1.004.827c.424.35.534.955.26 1.43l-1.298 2.247a1.125 1.125 0 0 1-1.369.491l-1.217-.456c-.355-.133-.75-.072-1.076.124a6.47 6.47 0 0 1-.22.128c-.331.183-.581.495-.644.869l-.213 1.281c-.09.543-.56.94-1.11.94h-2.594c-.55 0-1.019-.398-1.11-.94l-.213-1.281c-.062-.374-.312-.686-.644-.87a6.52 6.52 0 0 1-.22-.127c-.325-.196-.72-.257-1.076-.124l-1.217.456a1.125 1.125 0 0 1-1.369-.49l-1.297-2.247a1.125 1.125 0 0 1 .26-1.431l1.004-.827c.292-.24.437-.613.43-.991a6.932 6.932 0 0 1 0-.255c.007-.38-.138-.751-.43-.992l-1.004-.827a1.125 1.125 0 0 1-.26-1.43l1.297-2.247a1.125 1.125 0 0 1 1.37-.491l1.216.456c.356.133.751.072 1.076-.124.072-.044.146-.086.22-.128.332-.183.582-.495.644-.869l.214-1.28Z"
-        />
-        <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+          d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15m-3 0-3-3m0 0 3-3m-3 3H15"
         />
       </svg>
     </button>
@@ -70,13 +79,17 @@
             : 'Unbekannt'}
         </h1>
         <p class="text-sm text-gray-500 -mt-1">
+          {$currentRecording?.settings?.name}.mp3 |
           {lengthToString($currentRecording?.length || 0)} |
           {bytesToMegaBytesString($currentRecording?.size || 0)}
         </p>
       </div>
       <div>
         <button
-          on:click={() => stopRecording($currentRecording)}
+          on:click={() => {
+            stopRecording($currentRecording)
+            navigateTo('/recordings')
+          }}
           title="Aufnahme beenden"
           class="rounded-full h-8 w-8 bg-red-400 flex items-center justify-center"
         >
