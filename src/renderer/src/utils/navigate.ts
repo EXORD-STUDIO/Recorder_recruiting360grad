fdc550e50770f559e17436a9528b48e0f3ac34d4
Importantus <Importantus@users.noreply.github.com>
2025-01-23 15:08:02 +0100

    Fix removing home route

--- src/renderer/src/utils/navigate.ts
+++ src/renderer/src/utils/navigate.ts
@@ -4,6 +4,8 @@ import { route } from '../stores/router.store'
 export function navigateTo(path: string): void {
   // If no first / exists, add it
   path = path.startsWith('/') ? path : `/${path}`
+  // Remove trailing /
+  path = path.endsWith('/') && path.length > 1 ? path.slice(0, -1) : path
 
   if (path.startsWith('/login') || checkAuth()) {
     console.log('Navigating to', path)
